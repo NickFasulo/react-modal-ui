4586ac50325aaec3b4652cc1f8c96295433f4ecf
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-01-10 13:01:59 -0500

    recipe cards now rendering

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,23 +5,24 @@ import './app.css'
 export default function App() {
   const [recipes, setRecipes] = useState([])
 
-  useEffect(() => {
-    fetch(
-      'https://api.spoonacular.com/recipes/random?apiKey=3a542766a3e1449da68d750f40babf15&number=1'
+  const fetchData = async () => {
+    const response = await fetch(
+      'https://api.spoonacular.com/recipes/random?apiKey=3a542766a3e1449da68d750f40babf15&number=20'
     )
-      .then(res => res.json())
-      .then(recipes => setRecipes(recipes))
+    const data = await response.json()
+    setRecipes(data.recipes)
+  }
+
+  useEffect(() => {
+    fetchData()
   }, [])
 
   console.log(recipes)
 
   return (
     <div className='App'>
-      {recipes.map(({ ...recipes }, i) => (
-        <Card
-          key={i}
-          data={recipes}
-        />
+      {recipes.map((recipe, i) => (
+        <Card key={i} data={recipe} />
       ))}
     </div>
   )
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,10 @@
 import './card.css'
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <img src={props.data.image} alt={props.data.title} />
+      <h3>{props.data.title}</h3>
+    </div>
   )
 }
