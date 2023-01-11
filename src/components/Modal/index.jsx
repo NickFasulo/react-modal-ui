e2d1888669f78fede44daaf72e5da85e4ef67657
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-01-11 15:01:11 -0500

    add close modal func

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,7 +2,11 @@ import './modal.css'
 
 export default function Modal(props) {
   const { image, title } = props.data
+
   return (
-    <div className='modal'>{title}</div>
+    <div className='modal'>
+      <h3>{title}</h3>
+      <button onClick={props.onClose}>Close</button>
+    </div>
   )
 }
