23057cefe0986b3f50d2b4f913431bbe209751ba
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-01-11 16:37:56 -0500

    sanitize recipe summary html

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,12 +1,14 @@
+import parse from 'html-react-parser'
 import './modal.css'
 
 export default function Modal(props) {
-  const { image, title } = props.data
+  const { image, title, summary } = props.data
 
   return (
     <div className='modal'>
       <h3>{title}</h3>
       <button onClick={props.onClose}>Close</button>
+      <p>{parse(summary)}</p>
     </div>
   )
 }
