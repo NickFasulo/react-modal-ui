ec3398ceadfa28ea9aa37d66450be4bf71804302
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-01-12 10:08:36 -0500

    edit css and add title

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import Card from '../Card'
 import Modal from '../Modal'
-import recipeData from '../../data.json'
+// import recipeData from '../../data.json'
 import './app.css'
 
 export default function App() {
@@ -9,17 +9,17 @@ export default function App() {
   const [selectedRecipe, setSelectedRecipe] = useState({})
   const [open, setOpen] = useState(false)
 
-  // const fetchData = async () => {
-  //   const response = await fetch(
-  //     'https://api.spoonacular.com/recipes/random?apiKey=3a542766a3e1449da68d750f40babf15&number=20'
-  //   )
-  //   const data = await response.json()
-  //   setRecipes(data.recipes)
-  // }
+  const fetchData = async () => {
+    const response = await fetch(
+      'https://api.spoonacular.com/recipes/random?apiKey=3a542766a3e1449da68d750f40babf15&number=20'
+    )
+    const data = await response.json()
+    setRecipes(data.recipes)
+  }
 
   useEffect(() => {
-    // fetchData()
-    setRecipes(recipeData)
+    fetchData()
+    // setRecipes(recipeData)
   }, [])
 
   const handleOpen = recipe => {
@@ -35,11 +35,14 @@ export default function App() {
   console.log(selectedRecipe)
 
   return (
-    <div className='main'>
-      {recipes.map((recipe, i) => (
-        <Card key={i} data={recipe} onClick={() => handleOpen(recipe)} />
-      ))}
-      {open ? <Modal data={selectedRecipe} onClose={handleClose} /> : null}
-    </div>
+    <>
+      <h1>Random Recipes</h1>
+      <div className='main'>
+        {recipes.map((recipe, i) => (
+          <Card key={i} data={recipe} onClick={() => handleOpen(recipe)} />
+        ))}
+        {open ? <Modal data={selectedRecipe} onClose={handleClose} /> : null}
+      </div>
+    </>
   )
 }
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,13 +2,21 @@ import parse from 'html-react-parser'
 import './modal.css'
 
 export default function Modal(props) {
-  const { image, title, summary } = props.data
+  const { image, title, summary, instructions } = props.data
 
   return (
     <div className='modal'>
-      <h3>{title}</h3>
-      <button onClick={props.onClose}>Close</button>
-      <p>{parse(summary)}</p>
+      <div className='modal-content'>
+        <button onClick={props.onClose}>Close</button>
+        <div className='modal-summary'>
+          <div>
+            <h3>{title}</h3>
+            <img src={image ? image : '/placeholder.jpg'} alt={title} />
+          </div>
+          <span>{parse(summary)}</span>
+        </div>
+        <p className='instructions'>{parse(instructions)}</p>
+      </div>
     </div>
   )
 }
